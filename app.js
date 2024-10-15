d333b709c34929c3179f6342743ea23784408c9c
Sohail Khan <133193585+re-sohail@users.noreply.github.com>
2024-10-15 15:34:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/uploads", express.static(path.join(__dirname, "middleware/uploads")));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://podcastguru.netlify.app/"],
     credentials: true,
   })
 );
