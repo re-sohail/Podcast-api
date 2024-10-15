ff32137761cec5aa70487b2ee56d60a57b4caa2f
Sohail Khan <133193585+re-sohail@users.noreply.github.com>
2024-10-15 15:41:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "middleware/uploads")));
 
-app.use(
-  cors({
-    origin: ["https://podcastguru.netlify.app/"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: 'https://podcastguru.netlify.app', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', 
+  allowedHeaders: 'Content-Type,Authorization', 
+  credentials: true, 
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 app.use("/", userRoutes);
 app.use("/", categoryRoutes);
